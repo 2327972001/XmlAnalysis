5312848117339fe154550a3c5fba874f6faefc6a
ZouYangMing <2327972001@qq.com>
2022-05-12 14:48:53 +0800

    1、修复多层标签无法翻译 2、修复程序异常报错

--- src/main/java/com/zym/Start.java
+++ src/main/java/com/zym/Start.java
@@ -21,46 +21,88 @@ public class Start {
     public static void main(String[] args) throws DocumentException {
         //1.创建Reader对象
         SAXReader reader = new SAXReader();
-        //2.加载xml
         Scanner input_files = new Scanner(System.in);
         System.out.println("请输入需要解析的XML文件路径：");
         String file_path = input_files.nextLine();
-        System.out.print("请输入保存的TXT文件路径");
-        System.err.println("（注意：需要先创建一个txt文件）：");
+        System.out.print("请输入保存的TXT文件路径或名称");
+        System.err.println("（例如：a.txt 路径不能有中文）：");
         String save_path = input_files.nextLine();
+        //2.加载xml
         Document document = reader.read(new File(file_path));
         //3.获取根节点
         Element rootElement = document.getRootElement();
+        //获取第二层节点
         Iterator iterator = rootElement.elementIterator();
         String str = "";
         System.err.println("开始运行...请耐心等待...");
         while (iterator.hasNext()){
             Element stu = (Element) iterator.next();
             List<Attribute> attributes = stu.attributes();
-            for (Attribute attribute : attributes) {
-                if(attribute.getName() == "name"){
-                    String[] a = attribute.getValue().split("}");
-                    if(a.length == 2){
-                        RestTemplate restTemplate = new RestTemplate();
-                        String url = "https://api.tjit.net/api/fanyi/?key=BsmDcnNxJi24M8xKFwumRVw9gL&text="+a[1]+"&from=en&to=zh";
-                        JSONObject result = restTemplate.getForObject(url, JSONObject.class);
-                        String text = result.getString("data");
-                        text = text.substring(text.indexOf("dst=")+4,text.indexOf("}]}"));
-                        str += "<string id=\""+a[0].replace("{=","")+"\" text=\""+text+"\" />\n";
-                        //中文字符串校验
-                        if(!text.matches("[\\u4e00-\\u9fa5]+")){
-                            System.out.println("翻译失败，请手动翻译："+"<string id=\""+a[0].replace("{=","")+"\" text=\""+text+"\" />");
-                        }else{
-                            System.out.println("翻译成功："+"<string id=\""+a[0].replace("{=","")+"\" text=\""+text+"\" />");
+            //获取第三层节点
+            Iterator iterator2 = stu.elementIterator();
+            while (iterator2.hasNext()){
+                Element stu2 = (Element) iterator2.next();
+                //获取第四层节点
+                Iterator iterator3 = stu2.elementIterator();
+                while (iterator3.hasNext()){
+                    Element stu3 = (Element) iterator3.next();
+                    List<Attribute> attributes3 = stu3.attributes();
+                    for (Attribute attribute : attributes3) {
+                        //过滤多余标签
+                        if(attribute.getName() == "name" &&
+                                        (stu2.getName()!="child_character_templates" &&
+                                                stu2.getName()!="notable_and_wanderer_templates" &&
+                                                stu2.getName()!="lord_templates" &&
+                                                stu2.getName()!="rebellion_hero_templates" &&
+                                                stu2.getName()!="tournament_team_templates_one_participant" &&
+                                                stu2.getName()!="tournament_team_templates_two_participant" &&
+                                                stu2.getName()!="tournament_team_templates_four_participant")){
+                            String[] a = attribute.getValue().split("}");
+                            str += zh(attribute);
                         }
                     }
                 }
+            }
+            for (Attribute attribute : attributes) {
+                str += zh(attribute);
             }
         }
         writeFile(str,save_path);
         System.err.println("自动翻译成功，请查看："+save_path);
         System.out.println("按回车退出...");
         input_files.nextLine();
+    }
+
+    /**
+     * 翻译
+     * @param attribute
+     */
+    public static String zh(Attribute attribute) {
+        String str = "";
+        if(attribute.getName() == "name"){
+            String[] a = attribute.getValue().split("}");
+            //判断是否拿到value值
+            if(a.length == 2){
+                RestTemplate restTemplate = new RestTemplate();
+                String url = "https://api.tjit.net/api/fanyi/?key=BsmDcnNxJi24M8xKFwumRVw9gL&text="+a[1]+"&from=auto&to=zh";
+                JSONObject result = restTemplate.getForObject(url, JSONObject.class);
+                String text = result.getString("data");
+                if(text.indexOf("error_msg") == -1){
+                    text = text.substring(text.indexOf("dst=")+4,text.indexOf("}]}"));
+                    str = "<string id=\""+a[0].replace("{=","")+"\" text=\""+text+"\" />\n";
+                    //中文字符串校验
+                    if(!text.matches("[\\u4e00-\\u9fa5]+")){
+                        System.out.println("翻译失败，请手动翻译："+"<string id=\""+a[0].replace("{=","")+"\" text=\""+text+"\" />");
+                    }else{
+                        System.out.println("翻译成功："+"<string id=\""+a[0].replace("{=","")+"\" text=\""+text+"\" />");
+                    }
+                }else{
+                    System.out.println("连接超时，正在重试...");
+                    zh(attribute);
+                }
+            }
+        }
+        return str;
     }
 
     /**
